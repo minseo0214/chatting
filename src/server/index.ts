37a26bce879f0f5c51106e5ccab70b92c25a8b69
minseo0214 <minseo>
2022-06-02 18:07:27 +0900

    add except

--- src/server/index.ts
+++ src/server/index.ts
@@ -23,15 +23,22 @@ app.ws.use((ctx) => {
       time: new Date().toISOString(),
     }
 
+    console.log(clients.length)
+
     // TODO: 여기에서의 ctx는 지금 이 클로저 내의 한 개의 ctx에만 해당하기 때문에, 클라이언트 전부에게 보낼 수가 없다. 어떻게 해야 할까?
     for (const c of clients) {
       //C가 연결되었는지 확인.
-      if (c.websocket.readyState === c.websocket.CLOSED) continue
+      //if (c.websocket.readyState === c.websocket.CLOSED) continue
       c.websocket.send(JSON.stringify(msgToClient))
     }
   })
 
-  // ctx.websocket.on('close')
+  ctx.websocket.on('close', () => {
+    const closedIndex = clients.findIndex((c) => {
+      return c === ctx
+    })
+    if (closedIndex != -1) clients.splice(closedIndex, 1)
+  })
 })
 
 app.use(KoaStatic('./public'))
